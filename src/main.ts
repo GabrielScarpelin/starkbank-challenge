cda1b8bd9dbc2e0147e4c931873d6b05c1ab3d0c
GabrielScarpelinDiniz <159063078+GabrielScarpelinDiniz@users.noreply.github.com>
2024-11-01 13:49:19 -0300

    Adiciona as variaveis de ambiente para o https

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.KEY_HTTPS_PATH),
+    cert: fs.readFileSync(process.env.CERT_HTTPS_PATH),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(3000);
 }
 bootstrap();
