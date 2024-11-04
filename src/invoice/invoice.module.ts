8f2eaff2b21f7ea2cdd06727995fffc1c34fd4db
GabrielScarpelinDiniz <159063078+GabrielScarpelinDiniz@users.noreply.github.com>
2024-11-04 16:21:05 -0300

    fix: arruma erro de injeção de dependencia

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -3,9 +3,17 @@ import { StarkbankCallbackModule } from 'src/starkbank-callback/starkbank-callba
 import { PrismaService } from 'src/prisma/prisma.service';
 import { StarkbankConfig } from 'src/starkbank-integration/starkbank.config';
 import { InvoiceService } from './invoice.service';
+import { StarkbankCallbackService } from 'src/starkbank-callback/starkbank-callback.service';
+import { TransferService } from 'src/transfer/transfer.service';
 
 @Module({
   imports: [StarkbankCallbackModule],
-  providers: [InvoiceService, PrismaService, StarkbankConfig],
+  providers: [
+    InvoiceService,
+    PrismaService,
+    StarkbankConfig,
+    StarkbankCallbackService,
+    TransferService,
+  ],
 })
 export class InvoiceModule {}
